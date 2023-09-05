6a26c078ab1870118b271d8dd29c52e02bcbd8f2
marvinbra <marvinbopp37@gmail.com>
2023-09-05 09:19:25 +0200

    update handleOrder function

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
 import { menuArray } from "./data";
 
-class Feed {
-    constructor(data) {
-        Object.assign(this, data)
-    }
+const shoppingCart = document.getElementById("shoppingCart")
+const finishOrder = document.getElementById("finishOrder")
+const yourOrder = document.getElementById("your−order")
+const menuIndex = []
+let totalPrice = 0
 
-    getMenuHtml() {
-        const {name, ingredients, price, emoji, uuid} = this
-        return `
-        <div class="menuCard" id="menuCard">
-            <div class="item-details">
-                <div class="emoji"> ${menu} </div>
+document.addEventListener("click", function(e){
+    if(e.target.dataset.btn){
+        handleOrderClick(e.target.dataset.btn)
+    } if(e.target.dataset.removebtn){
+        handleRemoveClick(e.target.dataset.removebtn)
+    }
+})
+
+function handleOrderClick(orderid){
+    menuArray.forEach(function(menu){
+        if(menu.uuid === orderid){
+            totalPrice += menu.price
+            menuIndex.push(menu.id)
+            shoppingCart.innerHTML += `
+            <div class="addToOrder">
+                <div>
+                    <div class="orderItem"><span>${menu.name}</span></div>
+                    <button data-removebtn="${menu.uuid}">remove</button>
+                </div>
+                <div>
+                    <div>${menu.price}</div>
+                </div>
             </div>
-            <div class="info">
-                <div class="name"> ${name} </div>
-                <div class="ingredients"> ${ingredients} </div>
-                <div class="price"> ${price} </div>
-            </div>
-            <div>
-                <button id="buyBtn" data-btn="${uuid}">+</button
-            </div>
-        </div>
-        `
-    }    
+            `
+        } if (shoppingCart.innerHTML) {
+            finishOrder.innerHTML = `
+            <p>Total Price: </p>
+            <p>${totalPrice}</p>
+            <button>Complete order</button>
+            `
+            yourOrder.innerHTML = `
+            <p>Your Order</p>
+            `
+        }
+    })
 }
 
-let menuFeed = new Feed(menuArray[0])
+function handleRemoveClick(orderid){
+    menuArray.forEach(function(menu){
+        if(menu.uuid = orderid){
+            console.log(typeof menuIndex)
+        }
+    })
+}
+
+const menuFeed = menuArray.map(function(menu){
+//   const {name, ingredients, price, emoji, uuid} = menuArray
+    return`
+        <div class="menuCard" id="menuCard">
+            <div class="item-details">
+                <div class="emoji"> ${menu.emoji} </div>
+            </div>
+            <div class="info">
+                <div class="name"> ${menu.name} </div>
+                <div class="ingredients"> ${menu.ingredients} </div>
+                <div class="price"> ${menu.price} </div>
+            </div>
+            <div>
+                <button id="buyBtn" data-btn="${menu.uuid}">+</button
+            </div>
+        </div>
+    `
+})
 
 function render() {
-    document.getElementById("menu").innerHTML = menuFeed.getMenuHtml()
+    document.getElementById("menu").innerHTML = menuFeed
 }
 
 render()
