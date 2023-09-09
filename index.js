e6ceaef87040bb75c987c595ed3792e2661e28d7
marvinbra <marvinbopp37@gmail.com>
2023-09-09 13:07:16 +0200

    remove function fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import { menuArray } from "./data";
+import { v4 as uuidv4 } from 'uuid';
 
 const shoppingCart = document.getElementById("shoppingCart")
 const finishOrder = document.getElementById("finishOrder")
 const yourOrder = document.getElementById("your−order")
 const paymentModal = document.getElementById("payement-modal")
+const completeBtn = document.getElementById("completeBtn")
 const inputName = document.getElementById("input-name")
 const payBtn = document.getElementById("pay-btn")
 const thxMsg = document.getElementById("thanks-msg")
-const menuIndex = []
+let menuIndex = []
 let totalPrice = 0
 
 document.addEventListener("click", function(e){
@@ -21,62 +23,80 @@ document.addEventListener("click", function(e){
 })
 
 function handleOrderClick(orderid){
+   
     menuArray.forEach(function(menu){
         if(menu.uuid === orderid){
             totalPrice += menu.price
-            menuIndex.push(menu.id)
+            const uuid = uuidv4()
+            menuIndex.push({
+                ...menu,
+                uuid
+                })
             shoppingCart.innerHTML += `
-            <div class="addToOrder">
-                <div>
-                    <div class="orderItem"><span>${menu.name}</span></div>
-                    <button data-removebtn="${menu.uuid}">remove</button>
+                <div class="addToOrder">
+                    <div>
+                        <div class="orderItem"><span>${menu.name}</span></div>
+                        <button data-removebtn="${uuid}">remove</button>
+                    </div>
+                    <div>
+                        <div>${menu.price}</div>
+                    </div>
                 </div>
-                <div>
-                    <div>${menu.price}</div>
-                </div>
-            </div>
             `
         } if (shoppingCart.innerHTML) {
             finishOrder.innerHTML = `
-            <p>Total Price: </p>
-            <p>${totalPrice}</p>
-            <button  data-completebtn="${menu.uuid}">Complete order</button>
+                <p>Total Price: </p>
+                <p>${totalPrice}</p>
+                <button  data-completebtn="${menu.uuid}">Complete order</button>
             `
             yourOrder.innerHTML = `
-            <p>Your Order</p>
+                <p>Your Order</p>
             `
         }
     })
 }
 
-// not working
 function handleRemoveClick(orderid){
-    menuArray.forEach(function(menu){
-        if(menu.uuid = orderid){
-            shoppingCart.remove([menuIndex])
-        }
+    shoppingCart.innerHTML = ``
+    menuIndex = menuIndex.filter(function(menu){
+        return menu.uuid !== orderid
     })
+    menuIndex.forEach(menu => {
+        shoppingCart.innerHTML +=`
+                <div class="addToOrder">
+                    <div>
+                        <div class="orderItem"><span>${menu.name}</span></div>
+                        <button data-removebtn="${menu.uuid}">remove</button>
+                    </div>
+                    <div>
+                        <div>${menu.price}</div>
+                    </div>
+                </div>
+            `
+    })
+    
 }
 
 function handleCompleteClick(orderid){
     menuArray.forEach(function(menu){
         if(menu.uuid = orderid){
-            console.log("works")
             paymentModal.style.display = "flex"
         }
     })
 }
 
-payBtn.addEventListener("click", () => {
-    console.log("works")
+payBtn.addEventListener("click", function() {
     paymentModal.style.display = "none"
+    shoppingCart.textContent = ""
+    yourOrder.textContent = ""
+    finishOrder.textContent = ""
     thxMsg.innerHTML = `
     <p>Thanks ${inputName.value}! Your order is on its way.</p>
     `
 })
 
 const menuFeed = menuArray.map(function(menu){
-//   const {name, ingredients, price, emoji, uuid} = menuArray
+//  const {name, ingredients, price, emoji, uuid} = menuArray
     return`
         <div class="menuCard" id="menuCard">
             <div class="item-details">
